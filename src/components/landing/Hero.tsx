48b6a738d6d0a522758069215fef118c3c73e01e
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2025-12-27 13:12:46 +0530

    indent fix

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -3,11 +3,10 @@ const Hero = () => {
   return (
     <section className="text-white">
       <h1 className="text-4xl md:text-5xl font-headline font-bold leading-tight">
-        Exclusive Furniture — Straight from the Furniture Hub of Jodhpur
+        Exclusive Furniture - Straight from the Furniture Hub of Jodhpur
       </h1>
       <p className="mt-4 text-xl md:text-2xl font-headline text-accent">
-           1000+ Design Choices,  Factory-Direct Pricing, 
-World-Class Quality all at one roof.
+           1000+ Design Choices,  Factory-Direct Pricing, World-Class Quality all at one roof.
       </p>
     </section>
   );
